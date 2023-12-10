12aaebaba93cd2b1fdb1630c6aa244e5203470bd
xAlex2007 <c766ob@gmail.com>
2023-12-10 18:36:48 +0700

    init commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// переменные
+var y=0;
+let str = "hello";
+const bool = "привет";
+console.log(bool);
+
+// простые типы
+let num = 123345;
+console.log('num',typeof(num));
+let string1 = "Mikel";
+console.log ('string', typeof(string1));
+let bool1 = true;
+console.log('bool',typeof(bool1));
+let nath = null;
+let und = undefined;
+// BigInt;
+// Symbol;
+
+// сложные типы даннных
+const obj = {
+val:23,
+num: "234",
+arr: [1,32,45],
+name: {
+    firstname: "Egoroff",
+    lastname: "Ivan",
+    age: "35"
+}
+};
+// Array=[1,2,3,4,"lflsjjs;f",true];
+// function obj(){
+
+// };
+// Date;
+// RegExp;
+
